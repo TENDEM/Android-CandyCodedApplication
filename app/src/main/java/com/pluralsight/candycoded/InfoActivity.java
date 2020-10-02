8b9cbdbe0ab1107a7fdaabe2b504b272f1fd94e5
Emma <tcepuechi@gmail.com>
2020-10-02 21:52:55 +0100

    Google Map Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -40,4 +40,11 @@ public class InfoActivity extends AppCompatActivity {
         // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+        public void createPhoneIntent(View view) {
+            Uri uriAddress = Uri.parse("tel:0123456789");
+            Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+            dialIntent.setData(uriAddress);
+            startActivity(dialIntent);
+
+        }
 }
